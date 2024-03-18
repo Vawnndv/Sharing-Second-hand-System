435412c37838f5cbf8d7f859eb25e254fa24e2f8
ChauHoangTan <chauhoangtan6937@gmail.com>
2024-03-18 11:47:21 +0700

    fix eslint

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
     'import',
     '@typescript-eslint',
   ],
+  ignorePatterns: ['src/classDiagramModel/**'],
   rules: {
     'comma-dangle': 0,
     'no-underscore-dangle': 0,
@@ -28,7 +29,7 @@ module.exports = {
       '@typescript-eslint/parser': [
         '.ts',
         '.tsx',
-      ]
+      ],
     },
     'import/resolver': {
       typescript: {},
