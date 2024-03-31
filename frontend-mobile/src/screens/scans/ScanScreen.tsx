39a8e9397a5bc7f8160a2524cbeb1fb513fa2302
Vawnndv <dinhvanguyen810@gmail.com>
2024-03-31 14:39:12 +0700

    Create screen scan QR

--- frontend-mobile/src/screens/scans/ScanScreen.tsx
+++ frontend-mobile/src/screens/scans/ScanScreen.tsx
@@ -1,15 +1,63 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { ContainerComponent, HeaderComponent, SectionComponent } from '../../components'
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { ContainerComponent, SectionComponent } from '../../components'
 
-const ScanScreen = () => {
+export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data } : any) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Đang yêu cầu quyền cho Camera</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>Không truy cập được Camera</Text>;
+  }
+
   return (
-    <ContainerComponent isScroll title='Scan Screen' right back>
-      <SectionComponent>
-        <Text>ScanScreen</Text>
-      </SectionComponent>
+    <ContainerComponent>
+        <View style={styles.container}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+          {scanned && <Button title={'Chạm để quét lại lần nữa'} onPress={() => setScanned(false)} />}
+          <View style={styles.marker}/>
+        </View>
     </ContainerComponent>
-  )
+  );
 }
 
-export default ScanScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  marker: {
+    borderWidth: 2,
+    borderColor: '#FFFFFF',
+    borderRadius: 10,
+    width: 200,
+    height: 200,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: -100 }, { translateY: -100 }],
+    zIndex: 1, // Ensure the marker is above the camera preview
+  },
+});
