6fd095397a6556f371a2c7aa6c4ad739672f9362
Vawnndv <dinhvanguyen810@gmail.com>
2024-04-07 23:12:46 +0700

    Fix and update after merge

--- backend/src/routes/postRouter.ts
+++ backend/src/routes/postRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {  getPostDetails, getPostReceivers, createPost } from '../controllers/postController';
+import { getPostDetails, getPostReceivers, createPost, getAllPostFromUserPost, getAllPostFromWarehouse, searchPost } from '../controllers/postController';
 
 const router = express.Router();
 
