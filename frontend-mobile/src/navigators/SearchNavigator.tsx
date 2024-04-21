c29aec2072a40ede06c534f242254b2cbf7b4609
Vawnndv <dinhvanguyen810@gmail.com>
2024-04-21 22:50:51 +0700

    Update Search navigation from PostDetail

--- frontend-mobile/src/navigators/SearchNavigator.tsx
+++ frontend-mobile/src/navigators/SearchNavigator.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import SearchScreen from '../screens/search/SearchScreen';
 import ItemDetailScreen from '../screens/home/ItemDetailScreen';
+import SearchResultScreen from '../screens/search/SearchResultScreen';
 
 const SearchNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -9,6 +10,7 @@ const SearchNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Search" component={SearchScreen} />
+      <Stack.Screen name="SearchResultScreen" component={SearchResultScreen} />
       <Stack.Screen name="ItemDetailScreen" component={ItemDetailScreen} />
     </Stack.Navigator>
   )
