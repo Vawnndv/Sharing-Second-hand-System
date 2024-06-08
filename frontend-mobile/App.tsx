7d1567dea3c44eae04899f75a0a959eddc7b851f
LTPhiHung <phihung15032001@gmail.com>
2024-06-09 00:28:54 +0700

    add fcm notification

--- frontend-mobile/App.tsx
+++ frontend-mobile/App.tsx
@@ -24,10 +24,18 @@ import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import AppRouters from './src/screens/auth/AppRouters';
 import { registerRootComponent } from 'expo';
+import { usePushNotifications } from './src/utils/usePushNotification';
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
+
+
 export default function App() {
+  const {expoPushToken, notification} = usePushNotifications();
+
+  const data = JSON.stringify(notification, undefined, 2);
+  console.log("token: ", expoPushToken);
+  console.log("data: ", data);
   const [appIsReady, setAppIsReady] = useState(false);
   // const [fontsLoaded, setFontsLoaded] = useState(false);
 
@@ -95,3 +103,100 @@ export default function App() {
 }
 
 registerRootComponent(App);
+
+
+// import { Button, Platform, Text, View } from "react-native";
+// import { useState, useEffect } from "react";
+// import * as Device from "expo-device";
+// import * as Notifications from "expo-notifications";
+
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldShowAlert: true,
+//     shouldPlaySound: true,
+//     shouldSetBadge: false,
+//   }),
+// });
+
+// export default function App() {
+//   const [expoPushToken, setExpoPushToken] = useState("");
+
+//   useEffect(() => {
+//     console.log("Registering for push notifications...");
+//     registerForPushNotificationsAsync()
+//       .then((token: any) => {
+//         console.log("token: ", token);
+//         setExpoPushToken(token);
+//       })
+//       .catch((err) => console.log(err));
+//   }, []);
+
+//   async function registerForPushNotificationsAsync() {
+//     let token;
+
+//     if (Platform.OS === "android") {
+//       await Notifications.setNotificationChannelAsync("default", {
+//         name: "default",
+//         importance: Notifications.AndroidImportance.MAX,
+//         vibrationPattern: [0, 250, 250, 250],
+//         lightColor: "#FF231F7C",
+//       });
+//     }
+
+//     if (Device.isDevice) {
+//       const { status: existingStatus } =
+//         await Notifications.getPermissionsAsync();
+//       let finalStatus = existingStatus;
+//       if (existingStatus !== "granted") {
+//         const { status } = await Notifications.requestPermissionsAsync();
+//         finalStatus = status;
+//       }
+//       if (finalStatus !== "granted") {
+//         alert("Failed to get push token for push notification!");
+//         return;
+//       }
+//       // Learn more about projectId:
+//       // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+//       token = (
+//         await Notifications.getExpoPushTokenAsync({
+//           projectId: "3d1b1f78-a4b7-4ba8-9f74-8d08fdc8185d",
+//         })
+//       ).data;
+//       console.log(token);
+//     } else {
+//       alert("Must use physical device for Push Notifications");
+//     }
+
+//     return token;
+//   }
+
+//   const sendNotification = async () => {
+//     console.log("Sending push notification...");
+
+//     // notification message
+//     const message = {
+//       to: expoPushToken,
+//       sound: "default",
+//       title: "My first push notification!",
+//       body: "This is my first push notification made with expo rn app",
+//     };
+
+//     await fetch("https://exp.host/--/api/v2/push/send", {
+//       method: "POST",
+//       headers: {
+//         host: "exp.host",
+//         accept: "application/json",
+//         "accept-encoding": "gzip, deflate",
+//         "content-type": "application/json",
+//       },
+//       body: JSON.stringify(message),
+//     });
+//   };
+
+//   return (
+//     <View style={{ marginTop: 100, alignItems: "center" }}>
+//       <Text style={{ marginVertical: 30 }}>Expo RN Push Notifications</Text>
+//       <Button title="Send push notification" onPress={sendNotification} />
+//     </View>
+//   );
+// }
