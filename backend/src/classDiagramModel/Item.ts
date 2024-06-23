3d40a9efa4c5f044c40c6a3bcc2ad1fdcb51be66
ChauHoangTan <chauhoangtan6937@gmail.com>
2024-06-23 20:37:34 +0700

    rating and add model into collaborator mobile

--- backend/src/classDiagramModel/Item.ts
+++ backend/src/classDiagramModel/Item.ts
@@ -8,21 +8,24 @@ export class Item {
   private name: string | undefined;
 
   private quantity: number | undefined;
+  
+  private nametype: string | undefined;
 
   public itemtypeid: any;
     
-  public constructor(itemID: number, name: string, quantity: number, itemtypeid: number) {
+  public constructor(itemID: number, name: string, quantity: number, itemtypeid: number, nametype: string) {
     this.itemID = itemID;
     this.name = name;
     this.quantity = quantity;
     this.itemtypeid = itemtypeid;
+    this.nametype = nametype
   }
 
   static async getAllItems(): Promise<Item[] | null> {
     const client = await pool.connect();
     try {
       const result = await client.query('SELECT * FROM item');
-      return result.rows.map((row: any) => new Item(row.itemID, row.name, row.quantity, row.itemtypeid));
+      return result.rows.map((row: any) => new Item(row.itemID, row.name, row.quantity, row.itemtypeid, ''));
     } finally {
       return null;
     }
