0d081571d4a12d13d0d418fc56284fd35439d688
LTPhiHung <phihung15032001@gmail.com>
2024-06-28 22:56:47 +0700

    update fcm token for new expo

--- frontend-mobile/src/constants/appInfos.ts
+++ frontend-mobile/src/constants/appInfos.ts
@@ -1,7 +1,7 @@
 import { Dimensions } from "react-native";
 import { BASE_URL } from "@env";
 
-console.log('_BA_S_E_URL_________', BASE_URL)
+console.log('_BA_S_E_URL________', BASE_URL)
 
 export const appInfo = {
   sizes: {
